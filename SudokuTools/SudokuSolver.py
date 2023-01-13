0969bbeb84d52b06be66d40879d1658887f7d967
sebastiankane0xff@gmail.com <98116733+SebastianKane@users.noreply.github.com>
2023-01-13 13:03:18 -0800

    Cleaner Version
    Cleaner implementation that gets rid of the Column, Box and Row classes and instead relies on lists of references to other cells.

--- SudokuTools/SudokuSolver.py
+++ SudokuTools/SudokuSolver.py
@@ -1,97 +1,69 @@
-def get_column_id(index:int):
-    return index%9
-def get_row_id(index:int):
-    return index//9
-def get_box_id(index:int):
-    return get_column_id(index)//3+(get_row_id(index)//3)*3
-
-class Column:
-    def __init__(self,id:int):
-        self.id= id
-        self.values=set()
-
-    def __str__(self):
-        return f'Column {self.id} with values {self.values}.'
-
-    def possible_values(self):
-        return set(range(1,10)) - self.values
-
-    def add_value(self, value:int):
-        self.values.add(value)
-
-class Row:
-    def __init__(self,id:int):
-        self.id= id
-        self.values=set()
-
-    def __str__(self):
-        return f'Row {self.id} with values {self.values}.'
-
-    def possible_values(self):
-        return set(range(1,10)) - self.values
-
-    def add_value(self, value:int):
-        self.values.add(value)
-        
-class Box:
-    def __init__(self,id:int):
-        self.id=id
-        self.values=set()
-
-    def __str__(self):
-        return f'Box {self.id} with values {self.values}.'
-
-    def possible_values(self):
-        return set(range(1,10)) - self.values
-
-    def add_value(self, value:int):
-        self.values.add(value)
-
 class Cell:
-    def __init__(self, row:Row, column:Column, box:Box, value=0):
-        self.row=row
-        self.column=column
-        self.box=box
-        if value!= 0:
-            self.update_puzzle(value)
-        else:
-            self.value=value
+    def __init__(self, value=0):
+        self.in_column=[]
+        self.in_row=[]
+        self.in_box=[]
+        self.value=value
     
     def __str__(self):
-        return f'[value:{self.value}, column:{self.column}, row:{self.row}, box:{self.box}]'
+        return str(self.value)
 
     def __int__(self):
-        return self.value
+        return int(self.value)
     
     def update(self):
-        possible_values = set(range(1,10)) - self.column.values - self.row.values - self.box.values
-
+        possible_values = self.possible_values()
         if len(possible_values)==1:
-            self.update_puzzle(list(possible_values)[0])
+            self.value=list(possible_values)[0]
             return self.value
         else:
             return 0
+    
+    def possible_values(self):
+        column_values=set([int(cell) for cell in self.in_column])
+        row_values=set([int(cell) for cell in self.in_row])
+        box_values=set([int(cell) for cell in self.in_box])
+        return set(range(1,10)) - column_values - row_values - box_values
 
+    def set_column(self, column:list):
+        self.in_column=column
 
-    def update_puzzle(self, value:int):
-        self.column.add_value(value)
-        self.row.add_value(value)
-        self.box.add_value(value)
-        self.value=value
+    def set_row(self, row:list):
+        self.in_row=row
+
+    def set_box(self, box:list):
+        self.in_box=box
 
 class Puzzle:
     def __init__(self, numbers:str):
         '''
         numbers: A 81 digit integer that's used to denote a sudoku puzzle read left to right starting at the top going down to the bottom.
         '''
-        self.rows = [Row(x) for x in range(0,9)]
-        self.columns = [Column(x) for x in range(0,9)]
-        self.boxes = [Box(x) for x in range(0,9)]
         self.cells = []
-    
-        for index, num in enumerate(str(numbers)):
-            self.cells.append(Cell(self.rows[get_row_id(index)], self.columns[get_column_id(index)], self.boxes[get_box_id(index)], int(num)))
+        self.solutions=set()
 
+
+        columns = [[] for _ in range(9)]
+        rows = [[] for _ in range(9)]
+        boxes=[[] for _ in range(9)]
+        for index, num in enumerate(str(numbers)):
+            cell=Cell(num)
+            self.cells.append(cell)
+            column_num=index//9
+            row_num=index%9
+            columns[column_num].append(cell)
+            rows[row_num].append(cell)
+            boxes[column_num//3+(row_num//3)*3].append(cell)
+        
+        for x in range(9):
+            for cell in columns[x]:
+                cell.set_column(columns[x])
+
+            for cell in rows[x]:
+                cell.set_row(rows[x])
+
+            for cell in boxes[x]:
+                cell.set_box(boxes[x])
 
     def __int__(self) -> int:
         return int(''.join(str(int(x)) for x in self.cells))
